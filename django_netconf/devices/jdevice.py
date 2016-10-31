e5a814ef851bf8afba6ff8b2ac025dc99623367b
kabzoncheg <kabzonnigg@,ail.ru>
2016-10-31 19:07:33 +0300

    Initial commit. Raw project. Under development

--- django_netconf/devices/jdevice.py
+++ django_netconf/devices/jdevice.py
@@ -3,6 +3,48 @@ import os
 from jnpr.junos import Device
 
 
+class _CheckModel:
+
+    """
+    Class _CheckModel
+    Decorator class. When JunosDevices class instance has self.db_flag set to True,
+        this decorator performs check if the model name (if specified) refers to actual
+        Django model from ".models.py"
+    Works with class _Wrapper only. _Wrapper retains both instances _CheckModel and JunosDevice
+    """
+    def __init__(self, cmeth):
+        self.cmeth = cmeth
+
+    def __call__(self, *args, **kwargs):
+        cmeth_result = self.cmeth(*args, **kwargs)
+        if isinstance(cmeth_result, tuple):
+            pass
+        else:
+            return cmeth_result
+        if self.db_flag:
+            import pyclbr
+            model_names = [md for md in pyclbr.readmodule('models')]
+            if cmeth_result[1] in model_names:
+                return cmeth_result
+            else:
+                raise KeyError('Django model {} doesnot exist!'.format(cmeth_result[1]))
+        else:
+            return cmeth_result
+
+    def __get__(self, instance, owner):
+        return _Wrapper(self, instance)
+
+
+class _Wrapper:
+    def __init__(self, desc, subj):
+        self.desc = desc
+        self.subj = subj
+        self.desc.db_flag = subj.db_flag
+
+    def __call__(self, *args, **kwargs):
+        return self.desc(self.subj, *args, **kwargs)
+
+
 class JunosDevice:
 
     """Base class for Junos devices.
@@ -14,13 +56,15 @@ class JunosDevice:
     :attr:`connection`: connection to `hostname`
     """
 
-    @property
-    def route_table(self):
+    @_CheckModel
+    def get_route_table(self):
         """
         A list of routes bound to current route-tables
 
-        :return: list
+        :returns: list of routes
+        :rtype: list
         """
+        model = 'InstanceRouteTable'
         table = []
         root = self._connection.rpc.get_route_information(all=True)
         for route_table in root:
@@ -29,31 +73,44 @@ class JunosDevice:
                 entry = dict((elt.tag, elt.text) for elt in route.iter() if not len(elt) and elt.text is not None)
                 entry['table_name'] = table_name
                 table.append(entry)
-        return table
+        return table, model
 
-    @property
-    def interface_list(self):
+    @_CheckModel
+    def get_phy_interface_list(self):
         """
-        A list of interfaces with thier parameters
+        A list of physical interfaces with thier parameters
 
         :returns: list of interface
         :rtype: list
         """
+        model = 'InstancePhyInterface'
         table = []
         root = self._connection.rpc.get_interface_information()
-
         for phy_int in root:
             entry = dict(
                 (elt.tag, elt.text.strip()) for elt in phy_int.findall('./') if not len(elt) and elt.text is not None)
             entry['int-type'] = 'physical-interface'
             table.append(entry)
+        return table, model
 
+    @_CheckModel
+    def get_log_interface_list(self):
+        """
+        A list of logical interfaces with thier parameters
+
+        :returns: list of interface
+        :rtype: list
+        """
+        model = 'InstanceLogInterface'
+        table = []
+        root = self._connection.rpc.get_interface_information()
+        for phy_int in root:
             for log_int in phy_int.findall('logical-interface'):
                 entry = dict(
                     (elt.tag, elt.text.strip()) for elt in log_int.iter() if not len(elt) and elt.text is not None)
                 try:
                     unparsed_prefix = entry['ifa-destination']
-                except:
+                except KeyError:
                     pass
                 else:
                     parsed_prefix = unparsed_prefix.split('/').pop()
@@ -61,17 +118,17 @@ class JunosDevice:
                 finally:
                     entry['int-type'] = 'logical-interface'
                     table.append(entry)
+        return table, model
 
-        return table
-
-    @property
-    def route_instance_list(self):
+    @_CheckModel
+    def get_route_instance_list(self):
         """
         A list of routing instances with parameters
 
         :returns: list of routing instances
         :rtype: list
         """
+        model = 'DeviceInstance'
         table = []
         root = self._connection.rpc.get_instance_information(detail=True)
         for route_inst in root:
@@ -84,12 +141,14 @@ class JunosDevice:
                 if elt.tag == 'instance-rib':
                     entry['instance-rib_list'] = list(rib_elt.text for rib_elt in elt if rib_elt.tag == 'irib-name')
             table.append(entry)
-        return table
+        return table, model
 
-    @property
-    def facts(self):
-        return self._facts
+    @_CheckModel
+    def get_facts(self):
+        model = 'Device'
+        return self._facts, model
 
+    @_CheckModel
     def get_arp_table(self, vpn='default'):
         """
         A list of ARP entries.
@@ -97,6 +156,8 @@ class JunosDevice:
         :returns: ARP entries
         :rtype: list
         """
+
+        model = 'InstanceArpTable'
         table = []
         old_table = self._connection.rpc.get_arp_table_information(vpn=vpn)
         for old_entry in old_table:
@@ -108,7 +169,7 @@ class JunosDevice:
                          vpn=vpn,
                          mac_address=old_entry.findtext('mac-address').strip())
             table.append(entry)
-        return table
+        return table, model
 
     def __init__(self, *args, **kwargs):
         self.hostname = args[0] if len(args) else kwargs.get('host')
@@ -129,25 +190,25 @@ class JunosDevice:
         if self._connection:
             self._connection.close()
 
-    def _check_model(modelname='DOES NOT EXIST'):
-        import pyclbr
-        model_names = [md for md in pyclbr.readmodule('models')]
-        return model_names
-
-
-
 if __name__ == '__main__':
-    device = JunosDevice(host='10.0.1.1', password='Password12!', user='django')
-    # device.connect()
-    # inst = device.route_instance_list
-    # arp_t = device.get_arp_table()
-    # route_t = device.route_table
-    # int_l = device.interface_list
+    device = JunosDevice(host='10.0.1.1', password='Password12!', user='django',db_flag=True)
+    device.connect()
+    facts = device.get_facts()
+    inst = device.get_route_instance_list()
+    arp_t = device.get_arp_table()
+    route_t = device.get_route_table()
+    int_l = device.get_log_interface_list()
+    int_p = device.get_phy_interface_list()
     # for interface in int_l:
     #     if interface['name'] == 'ge-0/0/1.0':
     #         print(interface)
     #         break
     # print('INT_L', int_l)
     # device.disconnect()
-    x = device._check_model()
-    print(x)
+    # x = device._check_model()
+    # print(x)
+    # print(arp_t)
+    # print(facts)
+    # print(inst)
+    print(int_l)
+    print(int_p)

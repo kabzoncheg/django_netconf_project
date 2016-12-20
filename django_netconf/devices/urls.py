10af276725af1472ef0aadf92b948b95513e080d
kabzoncheg <kabzonnigg@,ail.ru>
2016-12-21 00:19:37 +0300

    Initial commit. Raw project. Under development

--- django_netconf/devices/urls.py
+++ django_netconf/devices/urls.py
@@ -6,4 +6,5 @@ app_name = 'devices'
 
 urlpatterns = [
     url(r'^$', views.DeviceListView.as_view(), name='index'),
+    url(r'^search/vrf_name/(?P<vrf_name>.+)/$', views.device_list_search_vrf_view, name='search_vrf'),
 ]
--- django_netconf/devices/views.py
+++ django_netconf/devices/views.py
@@ -7,12 +7,23 @@ from django.core.urlresolvers import reverse
 from django.utils import timezone
 
 from .models import Device
+from .models import DeviceInstance
+
 
 class DeviceListView(generic.ListView):
     model = Device
     template_name = 'devices/index.html'
     context_object_name = 'device_list'
-    # paginate_by = 10
 
     def get_queryset(self):
         return Device.objects.order_by('ip_address')
+
+
+def device_list_search_vrf_view(request, vrf_name):
+    vrf_list = DeviceInstance.objects.filter(instance_name=vrf_name)
+    if vrf_list:
+        ip_addr_list = list(vrf.related_device_id for vrf in vrf_list)
+        device_list = list(Device.objects.get(ip_address=ip_addr) for ip_addr in ip_addr_list)
+        return render(request, 'devices/index.html', {'device_list': device_list})
+    else:
+        raise Http404
